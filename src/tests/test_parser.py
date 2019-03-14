78818af36ae77482fea2a2ff9710ab54e76236a7
Marc-Aurele Coste <cmarc@Fedora.XPS15>
2019-03-14 09:53:52 +0100

    Add resources files for the rework of the tests.

--- src/tests/test_parser.py
+++ src/tests/test_parser.py
@@ -1,7 +1,59 @@
 import os
+from typing import Dict
 
 from sqla_filters.parser.yaml import YAMLFiltersParser
+from sqla_filters.nodes.logical import AndNode
+from sqla_filters.nodes.operational import (
+    EqNode,
+    NotEqNode,
+    NullNode,
+    NotNullNode,
+    GtNode,
+    GteNode,
+    LtNode,
+    LteNode,
+    InNode,
+    NotInNode,
+    ContainsNode,
+    LikeNode
+)
 
+
+class TestParserBase(object):
+    _resources: Dict = {}
+
+    def _get_parser(self, key: str) -> YAMLFiltersParser:
+        """Return a parser instance."""
+        file_path = os.path.join(
+            os.path.dirname(__file__),
+            self._resources[key]
+        )
+        json_data = open(file_path).read()
+        return YAMLFiltersParser(json_data)
+
+
+class TestYamlEquality(TestParserBase):
+    def setup_class(self):
+        self._resources['eq'] = 'resources/eq/eq.yml'
+        self._resources['noteq'] = 'resources/eq/noteq.yml'
+        self._resources['eq_rel'] = 'resources/eq/eq_rel.yml'
+        self._resources['noteq_rel'] = 'resources/eq/noteq_rel.yml'
+
+    def test_01_eq(self):
+        parser = self._get_parser('eq')
+        assert isinstance(parser.tree.root, AndNode)
+        assert len(parser.tree.root.childs) == 1
+        assert isinstance(parser.tree.root.childs[0], EqNode)
+        assert parser.tree.root.childs[0].attribute == 'name'
+        assert parser.tree.root.childs[0].value == 'Toto'
+
+    def test_02_noteq(self):
+        parser = self._get_parser('noteq')
+        assert isinstance(parser.tree.root, AndNode)
+        assert len(parser.tree.root.childs) == 1
+        assert isinstance(parser.tree.root.childs[0], NotEqNode)
+        assert parser.tree.root.childs[0].attribute == 'name'
+        assert parser.tree.root.childs[0].value == 'Toto'
 
 class TestYamlFilterParser(object):
     def test_1_raw_data(self):
